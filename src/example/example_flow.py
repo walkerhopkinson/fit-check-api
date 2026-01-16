06e949cb6fe36424c13a3f7aa2c2846b0b10e82f
walkerwalker-jpg <walkerhopkinson@gmail.com>
2026-01-16 15:25:10 -0700

    fixed example

--- src/example/example_flow.py
+++ src/example/example_flow.py
@@ -1,6 +1,6 @@
 from config import PATH_TO_DATABASE
 
-import test.test_client as retriever
+import example_client as retriever
 
 import polars as pl
 
@@ -22,7 +22,15 @@ parquet_path = PATH_TO_DATABASE + "/products.parquet"
 csv_path = PATH_TO_DATABASE + "/products.csv"
 
 df.write_parquet(parquet_path)
+
+#convert for csv
 #df.write_csv(csv_path)
+# Before writing, convert any 'List' or 'Struct' columns to String
+df_for_csv = df.with_columns(
+    pl.col(pl.List, pl.Struct).cast(pl.String)
+)
+
+df_for_csv.write_csv(csv_path)
 
 print("Products saved to: " + parquet_path)
 print("Products saved to: " + csv_path)
