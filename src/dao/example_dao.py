df9b6cb8bcf1919a720d18a10a5b92573517fbfc
walkerwalker-jpg <walkerhopkinson@gmail.com>
2026-01-23 20:52:09 -0700

    more dao

--- src/dao/example_dao.py
+++ src/dao/example_dao.py
@@ -1,3 +1,33 @@
+import polars as pl
+
 class ExampleDao: 
-    def __init__(self): 
-        pass
+    def __init__(self, input_path: str): 
+        self.path_to_folder = input_path
+        self.path_to_csv = self.path_to_folder + "/products.csv"
+        self.path_to_parquet = self.path_to_folder + "/products.parquet"
+
+
+    def get_df_from_csv_file(self): 
+        #looks at path returns the entire table from it
+        print("looking at " + self.path_to_csv)
+        
+        df = pl.read_csv(self.path_to_csv)
+        return df     
+    
+    
+    def overwrite_csv_file(self, df: pl.DataFrame):
+        df.write_csv(self.path_to_csv)
+        print("Overwritten at " + self.path_to_csv)
+
+    
+
+    def get_df_from_parquet_file(self):
+        print("looking at " + self.path_to_parquet)
+
+        df = pl.read_parquet(self.path_to_parquet)
+        return df
+    
+    
+    def overwrite_parquet_file(self, df: pl.DataFrame):
+        df.write_parquet(self.path_to_parquet)
+        print("Overwritten at " + self.path_to_parquet)
